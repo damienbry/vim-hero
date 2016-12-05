f26bc3150bb0f8bd96ca09023b27ddd78627c7aa
Orbmancer <damien.bry@gmail.com>
2016-12-05 15:04:31 +0100

    first awesome configuration ! :tada:

---
+++ README.md
@@ -0,0 +1,13 @@
+# Orbmancer's vim-hero config
+My attempt to become a true master in vim coding :muscle:
+
+### Why ?
+
+I'm sick of the IDEs. I use less than 10% of their potential for 100% of my CPU.
+I want to be closer to my terminal, code and run all my tools in a simpler manner.
+
+### Contributing
+
+Feel free to suggest me awesome plugins / keymaps / commands / techniques.
+I'm certainly not yet a *vim-hero*, I'm always learning !
+
---
+++ setup.sh
@@ -0,0 +1,18 @@
+echo "Ormancer Vim Hero Config"
+echo "Setup started..."
+
+echo "- installing color schemes"
+mkdir -p ~/.vim/colors
+cp -r ./colors ~/.vim
+
+echo "Installing plugins"
+echo "- installing plugin manager: vim-plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp ./vim-config ~/.vimrc
+
+#npm i -g jscs
+
+echo "Setup finished ! Do a :PlugInstall vim and let's roll \o/"
+
